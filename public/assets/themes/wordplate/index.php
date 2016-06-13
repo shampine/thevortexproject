b3cd21333cf5fd50b4f93d094f060bfa503520ec
patrickshampine <patrick@patrickshampine.com>
2016-06-12 21:22:03 -0700

    Tiny loop change

--- public/assets/themes/wordplate/index.php
+++ public/assets/themes/wordplate/index.php
@@ -7,7 +7,7 @@
             the_post(); ?>
 
             <div class="row">
-                <div class="col-sm-10 col-sm-offset-1">
+                <div class="col-sm-12">
                     <?php the_content(); ?>
                 </div>
             </div><?php
