7ef194ca4be0a98c8c491bfc5e8d693d6ada44bf
patrickshampine <patrick@patrickshampine.com>
2016-06-15 19:38:15 -0700

    Updates

--- public/assets/themes/wordplate/index.php
+++ public/assets/themes/wordplate/index.php
@@ -7,7 +7,7 @@
             the_post(); ?>
 
             <div class="row">
-                <div class="col-sm-12">
+                <div class="col-sm-12 content">
                     <?php the_content(); ?>
                 </div>
             </div><?php
